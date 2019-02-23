c6c772054a0f5fdc817be3b122bcbbf1446a842e
Michael Wnek <michaelwnek1@gmail.com>
2019-02-23 15:09:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,10 @@
-if speak_to_grandma
+def speak_to_grandma(string)
+  if string != string.upcase
+    return "HUH ?! SPEAK UP SONNY!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+end
 
 
 
