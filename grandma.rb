75e6a06fedf8262fefc93145c9aecbd4be0fe845
Michael Wnek <michaelwnek1@gmail.com>
2019-02-23 15:15:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else string == string ("I LOVE YOU GRANDMA!")
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
   
